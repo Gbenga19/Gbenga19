7d61f33b436e5cc9fff4103e9a626d5dc8725f34
Gbenga19 <61985187+Gbenga19@users.noreply.github.com>
2020-03-23 23:48:54 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let name="Gbenga Odedina";
+
+let courses=("HTML","CSS","JAVASCRIPT");
+
+console.log("My name is:" +name);
+
+console.log(courses);
+
+const data=[]
+
+for (let i=0;i<=200; i++) {
+    if (i%2===1) {
+
+        data.push(i);
+    }
+
+}
+console.log(data);
